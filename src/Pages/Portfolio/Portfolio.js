1b50840c71b71e2d11436a603c6625f9d0aeb262
programmerNizam <mdnizam12109@gmail.com>
2022-05-26 12:05:00 +0600

    Create my portfolio page

--- src/Pages/Portfolio/Portfolio.js
+++ src/Pages/Portfolio/Portfolio.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React from "react";
+import me from "../../assets/me.png";
 
 const Portfolio = () => {
-    return (
-        <div>
-            
+  return (
+    <section className="bg-accent p-10">
+      <div class="card lg:card-side shadow-xl rounded-none bg-secondary">
+        <figure>
+          <img className="" src={me} alt="Album" />
+        </figure>
+        <div class="card-body text-white">
+          <h2 class="text-2xl font-bold">Name: Md. Nizam Uddin</h2>
+          <h3 className="text-xl">Email Address: mdnizam12109@gmail.com</h3>
+          <h3 className="text-xl">
+            Education: Diploma in computer engineering, 7th semester
+          </h3>
+          <h3 className="text-xl">
+            Institute: BCI ENGINEERING INSTITUTE, Dhaka 1207
+          </h3>
+          <div class="card-actions justify-end">
+            <button class="btn btn-primary">Listen</button>
+          </div>
         </div>
-    );
+      </div>
+    </section>
+  );
 };
 
 export default Portfolio;
